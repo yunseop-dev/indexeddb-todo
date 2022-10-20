bf25e4ff5ff10af5c1d3dc85a965d242b40c4455
Yunseop Kim <yunseop.kim@actionpower.kr>
2022-10-20 17:28:35 +0900

    #1 dexie with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useState } from "react";
+import { databaseInstance } from './db/index';
+import { useLiveQuery } from 'dexie-react-hooks';
 
 function App() {
+  const [text, setText] = useState('');
+  const [isOrdered, setIsOrdered] = useState(false);
+  const db = useMemo(() => databaseInstance, []);
+  const todos = useLiveQuery(() => {
+    return isOrdered ? db.todos.orderBy('created').reverse().toArray() : db.todos.toArray();
+  }, [isOrdered]);
+
+  const onSubmitAdd = useCallback(async () => {
+    await db.todos.add({
+      text: text, done: false, created: Date.now()
+    })
+  }, [db.todos, text]);
+
+  const onToggleDone = useCallback(async (id: number) => {
+    const item = await db.todos.get(id);
+    if (item) {
+      await db.todos.update(id, { done: !item.done })
+    } else {
+      alert('item not found!');
+    }
+  }, [db])
+
+  const onClickDeleteItem = useCallback(async (id: number) => {
+    await db.todos.delete(id);
+  }, [db]);
+
+  const onClickOrder = useCallback(() => {
+    setIsOrdered(val => !val);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello IndexedDB!</h1>
+      <form onSubmit={onSubmitAdd}>
+        <h2>dexie with hooks</h2>
+        <label htmlFor="todo-input">할 일 입력</label>
+        <input id="todo-input" type="text" onChange={(e) => setText(e.target.value)} value={text} />
+        <button type="submit">
+          Add
+        </button>
+      </form>
+      <button type="button" onClick={onClickOrder}>정렬</button>
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>
+            <input id={todo.id?.toString()} type="checkbox" onChange={() => onToggleDone(todo.id as any)} checked={todo.done} />
+            <label htmlFor={todo.id?.toString()}>
+              <span style={{ textDecoration: todo.done ? 'line-through' : '' }}>
+                {todo.text} / {todo.created}
+              </span>
+            </label>
+            <button type="button" onClick={() => onClickDeleteItem(todo.id as any)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
